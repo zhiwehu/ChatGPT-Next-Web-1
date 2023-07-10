4a4d335548a565c4d756883bea6f2665ba4f9caa
Jeffrey Hu <zhiwehu@gmail.com>
2023-07-10 15:16:50 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "AI炼丹阁",
+  description: "人工智能私人助理",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "AI炼丹阁",
     statusBarStyle: "default",
   },
 };
